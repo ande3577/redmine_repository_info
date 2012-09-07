2a7ae8bff6af2d08ab8d26173580d2b309452c00
David S Anderson <ande3577@gmail.com>
2012-09-06 23:44:49 -0500

    Initial commit, displays repository title in issue

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_git_branch do
+  name 'Redmine Git Branch plugin'
+  author 'David S Anderson'
+  description 'Make viewing a git revision repository/branch aware.'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
